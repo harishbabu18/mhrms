bfd667cbc52aee2760e9a90e15fd5b8d4eaae3a0
harish <hkrishnan@qwinix.io>
2019-07-10 19:04:51 +0530

    Consume Rest Service

---
+++ lib/model/User.dart
@@ -0,0 +1,36 @@
+class User{
+  /*final int userId;
+  final int id;
+  final String title;
+  final String body;
+
+  User({this.userId, this.id, this.title, this.body});
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      userId: json['userId'],
+      id: json['id'],
+      title: json['title'],
+      body: json['body'],
+    );
+  }*/
+
+final int id;
+  final String firstName;
+  final String lastName;
+  final String email;
+  final String mobile;
+  final String password;
+
+  User({this.id, this.firstName, this.lastName, this.email, this.mobile,
+    this.password});
+  factory User.fromJson(Map<String,dynamic> json){
+    return User(id:json['id'],
+                firstName: json['firstName'],
+                lastName: json['lastName'],
+                email: json['email'],
+                mobile: json['mobile'],
+                password: json['password'],);
+
+  }
+}
---
+++ lib/screens/UserList.dart
@@ -0,0 +1,61 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:mhrms/model/User.dart';
+import 'package:http/http.dart' as http;
+
+
+class UserList extends StatelessWidget {
+
+  final List<String> entries = <String>['A', 'B', 'C', 'B', 'C', 'B', 'C', 'B', 'C', 'B', 'C', 'B', 'C', 'B', 'C', 'B', 'C', 'B', 'C', 'B', 'C', 'B', 'C', 'B', 'C', 'B', 'C', 'B', 'C'];
+  final List<int> colorCodes = <int>[600, 500, 100, 500, 100, 500, 100, 500, 100, 500, 100, 500, 100, 500, 100, 500, 100, 500, 100, 500, 100, 500, 100, 500, 100, 500, 100, 500, 100];
+
+  Future<User> fetchPost() async {
+    final response =
+    await http.get('http://192.168.2.87:8080/user/1');
+
+    if (response.statusCode == 200) {
+      // If server returns an OK response, parse the JSON.
+      return User.fromJson(json.decode(response.body));
+
+    } else {
+      // If that response was not OK, throw an error.
+      throw Exception('Failed to load post');
+    }
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: FutureBuilder<User>(
+          future: fetchPost(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Text(snapshot.data.firstName);
+            } else if (snapshot.hasError) {
+              return Text("${snapshot.error}");
+            }
+
+            // By default, show a loading spinner.
+            return CircularProgressIndicator();
+          },
+        ),
+      ),/*new ListView.builder(
+          padding: const EdgeInsets.all(8.0),
+          itemCount: entries.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Container(
+              height: 50,
+              color: Colors.amber[colorCodes[index]],
+              child: Center(child: Text('Entry ${entries[index]}')),
+            );
+          }
+      )
+      ,*/
+    );
+  }
+
+}
+  
